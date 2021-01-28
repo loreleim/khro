236a2e14f801c4a32646d97b17bf8a9b91a33731
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2021-01-28 00:35:00 -0800

    temp removal of skip content

--- header.php
+++ header.php
@@ -22,4 +22,3 @@ if ( ! defined( 'ABSPATH' ) ) {
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'khro' ); ?></a>
