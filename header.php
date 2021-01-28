3ab90c7d6aef4f78a13e1a6987230c6f81d02de8
loreleim <lorelei.miyamura@gmail.com>
2021-01-28 15:19:20 -0800

    Gets the custom header from WP admin

--- header.php
+++ header.php
@@ -16,7 +16,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial scale=1">
-	<?php wp_head(); ?>
+	<!--Custom Header-->
+  <?php if ( get_header_image() ) : ?>
+    <div id="site-header">
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+            <img src="<?php header_image(); ?>" width="<?php echo absint( get_custom_header()->width ); ?>" height="<?php echo absint( get_custom_header()->height ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
+        </a>
+    </div>
+  <?php endif; ?>
 </head>
 
 <body <?php body_class(); ?>>
