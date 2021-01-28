901b6afed2dcdc6e0e0b8868850e348f5bd1a422
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2021-01-28 00:15:23 -0800

    adding in footer authorship

--- footer.php
+++ footer.php
@@ -13,16 +13,9 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'khro' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'khro' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'khro' ), 'khro', '<a href="http://underscores.me/">Underscores.me</a>' );
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'khro' ), 'khro', '<a href="https://github.com/loreleim">LOE, LLC</a>' );
 				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
