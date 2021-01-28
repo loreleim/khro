ba16173239e611ae37392cb824e88e0ccd45e0c8
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2021-01-28 00:34:44 -0800

    lightweight template naming

--- header.php
+++ header.php
@@ -1,10 +1,6 @@
 <?php
 /**
- * The header for our theme
- *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ * Template for header display
  *
  * @package khro
  */
